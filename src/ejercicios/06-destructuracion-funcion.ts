df7442e0f1e0462af2b80912a6164e553ac46953
Matias98cs <matias98cs@gmail.com>
2023-01-04 19:32:57 -0300

    06 destructuracion con funciones

---
+++ src/ejercicios/06-destructuracion-funcion.ts
@@ -0,0 +1,36 @@
+
+
+// console.log('Hola Mundo! Matias');
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    desc: 'Ipone',
+    precio: 400
+}
+
+const tableta: Producto = {
+    desc: 'Apple Ipad',
+    precio: 350
+}
+
+function calculaISV(productos: Producto[]): [number, number] {
+    let total = 0
+    productos.forEach(({precio}) => {
+        total += precio
+    })
+
+    return [total ,total * 0.15]
+}
+const articulos = [telefono, tableta]
+
+const [total, isv] = calculaISV(articulos)
+console.log(`total: ${total}`)
+console.log(`ISV: ${isv}`)
