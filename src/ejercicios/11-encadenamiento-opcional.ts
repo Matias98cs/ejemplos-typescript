f7685757e43c0008a300618931618dd0f788e0a6
Matias98cs <matias98cs@gmail.com>
2023-01-08 19:57:09 -0300

    11 encadenamiento opcional

---
+++ src/ejercicios/11-encadenamiento-opcional.ts
@@ -0,0 +1,26 @@
+
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Pasajero {
+    nombre: string
+    hijos?: string[]
+}
+
+const pasajero1: Pasajero = {
+    nombre: 'Matias',
+}
+
+const pasajero2: Pasajero = {
+    nombre: 'Luis',
+    hijos: ['Natalia', 'Gabriel']
+}
+
+function imprimeHijos(pasajero: Pasajero):void {
+    const cuantosHijos = pasajero.hijos?.length || 0
+
+    console.log(cuantosHijos)
+}
+
+imprimeHijos(pasajero1)
